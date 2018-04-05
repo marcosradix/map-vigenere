a4534094981652c345ac96041042fce37c32fac6
marcosradix <marcosradix@gmail.com>
2018-04-05 17:25:35 -0300

    frontend concluido

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace map_vigenere.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
