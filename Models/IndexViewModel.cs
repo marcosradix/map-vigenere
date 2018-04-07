4e7056c7ad6dce2ad7c85395fe89f30c394830d6
marcosradix <marcosradix@gmail.com>
2018-04-06 21:05:17 -0300

    métodos mudados para regras de negócio model

--- Models/IndexViewModel.cs
+++ Models/IndexViewModel.cs
@@ -8,6 +8,42 @@ namespace map_vigenere.Models
         public string saidaTextoCifrado { get; set; }
          public string chave { get; set; }
         public bool verificar { get; set; }
-       
+        public static int alfabeto {get; set;} = 26;
+    private static int Mod(int a, int b)
+    {
+        return (a % b + b) % b;
+    }
+    public static string Cipher(string entrada, string chave, bool encipher)
+    {
+	for (int i = 0; i < chave.Length; ++i)
+		if (!char.IsLetter(chave[i]))
+			return null; // Error
+
+	string saida = string.Empty;
+	int contador = 0;
+
+	for (int i = 0; i < entrada.Length; ++i)
+	{
+		if (char.IsLetter(entrada[i]))
+		{
+			bool deixarTextoUpper = char.IsUpper(entrada[i]);
+			char padrao = deixarTextoUpper ? 'A' : 'a';
+			int chaveIndex = (i - contador) % chave.Length;
+			int k = (deixarTextoUpper ? char.ToUpper(chave[chaveIndex]) : char.ToLower(chave[chaveIndex])) - padrao;
+			k = encipher ? k : -k;
+			char ch = (char)((Mod(((entrada[i] + k) - padrao), alfabeto)) + padrao);
+			saida += ch;
+		}
+		else
+		{
+			saida += entrada[i];
+			++contador;
+		}
+	}
+
+	return saida;
+    }
+
+
     }
 }
